213eb4cb35e922b4e5ab271aeb3afdcb378f1360
raphaelvolz <rv@nogago.com>
2017-06-29 23:18:23 +0200

    R72 3.1.3.4

--- OutdoorGPS/MyTracks/src/com/google/android/apps/mytracks/ImportAsyncTask.java
+++ OutdoorGPS/MyTracks/src/com/google/android/apps/mytracks/ImportAsyncTask.java
@@ -43,246 +43,245 @@ import org.xml.sax.SAXException;
 
 /**
  * AsyncTask to import GPX files from the SD card.
- * 
+ *
  * @author Jimmy Shih
  */
 public class ImportAsyncTask extends AsyncTask<Void, Integer, Boolean> {
 
-  private static final String TAG = ImportAsyncTask.class.getSimpleName();
+    private static final String TAG = ImportAsyncTask.class.getSimpleName();
 
-  private ImportActivity importActivity;
-  private final boolean importAll;
-  private final String name;
-  private final Uri fileName;
-  private final MyTracksProviderUtils myTracksProviderUtils;
-  private WakeLock wakeLock;
+    private ImportActivity importActivity;
+    private final boolean importAll;
+    private final String name;
+    private final Uri fileName;
+    private final MyTracksProviderUtils myTracksProviderUtils;
+    private WakeLock wakeLock;
 
-  // true if the AsyncTask result is success
-  private boolean success;
+    // true if the AsyncTask result is success
+    private boolean success;
 
-  // true if the AsyncTask has completed
-  private boolean completed;
+    // true if the AsyncTask has completed
+    private boolean completed;
 
-  // number of files successfully imported
-  private int successCount;
+    // number of files successfully imported
+    private int successCount;
 
-  // number of files to import
-  private int totalCount;
+    // number of files to import
+    private int totalCount;
 
-  // the last successfully imported track id
-  private long trackId;
+    // the last successfully imported track id
+    private long trackId;
 
-  /**
-   * Creates an AsyncTask.
-   * 
-   * @param importActivity the activity currently associated with this AsyncTask
-   * @param importAll true to import all GPX files
-   * @param path path to import GPX files
-   */
-  public ImportAsyncTask(ImportActivity importActivity, boolean importAll, String name,
-      final Uri file) {
-    this.importActivity = importActivity;
-    this.importAll = importAll;
-    this.name = name;
-    this.fileName = file;
+    /**
+     * Creates an AsyncTask.
+     *
+     * @param importActivity the activity currently associated with this AsyncTask
+     * @param importAll      true to import all GPX files
+     * @param path           path to import GPX files
+     */
+    public ImportAsyncTask(ImportActivity importActivity, boolean importAll, String name,
+                           final Uri file) {
+        this.importActivity = importActivity;
+        this.importAll = importAll;
+        this.name = name;
+        this.fileName = file;
 
-    myTracksProviderUtils = MyTracksProviderUtils.Factory.get(importActivity);
+        myTracksProviderUtils = MyTracksProviderUtils.Factory.get(importActivity);
 
-    // Get the wake lock if not recording or paused
-    if (PreferencesUtils.getLong(importActivity, R.string.recording_track_id_key) == PreferencesUtils.RECORDING_TRACK_ID_DEFAULT
-        || PreferencesUtils.getBoolean(importActivity, R.string.recording_track_paused_key,
-            PreferencesUtils.RECORDING_TRACK_PAUSED_DEFAULT)) {
-      wakeLock = SystemUtils.acquireWakeLock(importActivity, wakeLock);
-    }
-
-    success = false;
-    completed = false;
-    successCount = 0;
-    totalCount = 0;
-    trackId = -1L;
-  }
-
-  /**
-   * Sets the current {@link ImportActivity} associated with this AyncTask.
-   * 
-   * @param importActivity the current {@link ImportActivity}, can be null
-   */
-  public void setActivity(ImportActivity importActivity) {
-    this.importActivity = importActivity;
-    if (completed && importActivity != null) {
-      importActivity.onAsyncTaskCompleted(success, successCount, totalCount, trackId);
-    }
-  }
-
-  @Override
-  protected void onPreExecute() {
-    if (importActivity != null) {
-      importActivity.showProgressDialog();
-    }
-  }
-
-  @Override
-  protected Boolean doInBackground(Void... params) {
-    try {
-      if (!FileUtils.isSdCardAvailable()) {
-        return false;
-      }
-
-      List<File> files = new ArrayList<File>();
-      File file1;
-      if (fileName != null) {
-        file1 = new File(fileName.getPath());}
-        else {
-        file1 = new File(FileUtils.buildExternalDirectoryPath("gpx"));  
+        // Get the wake lock if not recording or paused
+        if (PreferencesUtils.getLong(importActivity, R.string.recording_track_id_key) == PreferencesUtils.RECORDING_TRACK_ID_DEFAULT
+                || PreferencesUtils.getBoolean(importActivity, R.string.recording_track_paused_key,
+                PreferencesUtils.RECORDING_TRACK_PAUSED_DEFAULT)) {
+            wakeLock = SystemUtils.acquireWakeLock(importActivity, wakeLock);
         }
-        
-        if (importAll) {
-          File[] candidates = file1.listFiles();
-          if (candidates != null) {
-            for (File candidate : candidates) {
-              if (!candidate.isDirectory() && candidate.getName().endsWith(".gpx")) {
-                files.add(candidate);
-              }
+
+        success = false;
+        completed = false;
+        successCount = 0;
+        totalCount = 0;
+        trackId = -1L;
+    }
+
+    /**
+     * Sets the current {@link ImportActivity} associated with this AyncTask.
+     *
+     * @param importActivity the current {@link ImportActivity}, can be null
+     */
+    public void setActivity(ImportActivity importActivity) {
+        this.importActivity = importActivity;
+        if (completed && importActivity != null) {
+            importActivity.onAsyncTaskCompleted(success, successCount, totalCount, trackId);
+        }
+    }
+
+    @Override
+    protected void onPreExecute() {
+        if (importActivity != null) {
+            importActivity.showProgressDialog();
+        }
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        try {
+            if (!FileUtils.isSdCardAvailable()) {
+                return false;
             }
-          }
-        } else
-          files.add(file1);
-      
 
-     
-      totalCount = files.size();
-      if (totalCount == 0) {
-        return true;
-      }
-      if (totalCount == 1) {
-        // handle as Uri
-        importUri(fileName);
-        successCount++;
-      } else {
-        for (int i = 0; i < totalCount; i++) {
-          if (isCancelled()) {
-            // If cancelled, return true to show the number of files imported
+            List<File> files = new ArrayList<File>();
+            File file1;
+            if (fileName != null) {
+                file1 = new File(fileName.getPath());
+            } else {
+                file1 = new File(FileUtils.buildExternalDirectoryPath("gpx"));
+            }
+
+            if (importAll) {
+                File[] candidates = file1.listFiles();
+                if (candidates != null) {
+                    for (File candidate : candidates) {
+                        if (!candidate.isDirectory() && candidate.getName().endsWith(".gpx")) {
+                            files.add(candidate);
+                        }
+                    }
+                }
+            } else
+                files.add(file1);
+
+
+            totalCount = files.size();
+            if (totalCount == 0) {
+                return true;
+            }
+            if (totalCount == 1) {
+                // handle as Uri
+                importUri(fileName);
+                successCount++;
+            } else {
+                for (int i = 0; i < totalCount; i++) {
+                    if (isCancelled()) {
+                        // If cancelled, return true to show the number of files imported
+                        return true;
+                    }
+                    File file = files.get(i);
+                    if (importFile(file)) {
+                        successCount++;
+                    }
+                    publishProgress(i + 1, totalCount);
+                }
+            }
             return true;
-          }
-          File file = files.get(i);
-          if (importFile(file)) {
-            successCount++;
-          }
-          publishProgress(i + 1, totalCount);
+        } finally {
+            // Release the wake lock if obtained
+            if (wakeLock != null && wakeLock.isHeld()) {
+                wakeLock.release();
+            }
         }
-      }
-      return true;
-    } finally {
-      // Release the wake lock if obtained
-      if (wakeLock != null && wakeLock.isHeld()) {
-        wakeLock.release();
-      }
     }
-  }
 
-  @Override
-  protected void onProgressUpdate(Integer... values) {
-    if (importActivity != null) {
-      importActivity.setProgressDialogValue(values[0], values[1]);
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        if (importActivity != null) {
+            importActivity.setProgressDialogValue(values[0], values[1]);
+        }
     }
-  }
 
-  @Override
-  protected void onPostExecute(Boolean result) {
-    success = result;
-    completed = true;
-    if (importActivity != null) {
-      importActivity.onAsyncTaskCompleted(success, successCount, totalCount, trackId);
+    @Override
+    protected void onPostExecute(Boolean result) {
+        success = result;
+        completed = true;
+        if (importActivity != null) {
+            importActivity.onAsyncTaskCompleted(success, successCount, totalCount, trackId);
+        }
     }
-  }
 
-  /**
-   * Imports a GPX file.
-   * 
-   * @param file the file
-   */
-  private boolean importFile(final File file) {
-    try {
-      int minRecordingDistance = PreferencesUtils.getInt(importActivity,
-          R.string.min_recording_distance_key, PreferencesUtils.MIN_RECORDING_DISTANCE_DEFAULT);
-      long trackIds[] = GpxImporter.importGPXFile(new FileInputStream(file), myTracksProviderUtils,
-          minRecordingDistance);
-      int length = trackIds.length;
-      if (length > 0) {
-        trackId = trackIds[length - 1];
-      }
-      return true;
-    } catch (FileNotFoundException e) {
-      Log.d(TAG, "file: " + file.getAbsolutePath(), e);
-      return false;
-    } catch (ParserConfigurationException e) {
-      Log.d(TAG, "file: " + file.getAbsolutePath(), e);
-      return false;
-    } catch (SAXException e) {
-      Log.d(TAG, "file: " + file.getAbsolutePath(), e);
-      return false;
-    } catch (IOException e) {
-      Log.d(TAG, "file: " + file.getAbsolutePath(), e);
-      return false;
-    }
-  }
-
-  private boolean importUri(final Uri uri) {
-    InputStream is = null;
-
-    try {
-
-      final ParcelFileDescriptor pFD = importActivity.getContentResolver().openFileDescriptor(uri,
-          "r");
-
-      if (pFD != null) {
-
-        is = new FileInputStream(pFD.getFileDescriptor());
-
+    /**
+     * Imports a GPX file.
+     *
+     * @param file the file
+     */
+    private boolean importFile(final File file) {
         try {
-          int minRecordingDistance = PreferencesUtils.getInt(importActivity,
-              R.string.min_recording_distance_key, PreferencesUtils.MIN_RECORDING_DISTANCE_DEFAULT);
-          long trackIds[] = GpxImporter.importGPXFile(is, myTracksProviderUtils,
-              minRecordingDistance);
-          int length = trackIds.length;
-          if (length > 0) {
-            trackId = trackIds[length - 1];
-          }
-          return true;
+            int minRecordingDistance = PreferencesUtils.getInt(importActivity,
+                    R.string.min_recording_distance_key, PreferencesUtils.MIN_RECORDING_DISTANCE_DEFAULT);
+            long trackIds[] = GpxImporter.importGPXFile(new FileInputStream(file), myTracksProviderUtils,
+                    minRecordingDistance);
+            int length = trackIds.length;
+            if (length > 0) {
+                trackId = trackIds[length - 1];
+            }
+            return true;
         } catch (FileNotFoundException e) {
-          Log.d(TAG, "file: " + uri.toString(), e);
-          return false;
+            Log.d(TAG, "file: " + file.getAbsolutePath(), e);
+            return false;
         } catch (ParserConfigurationException e) {
-          Log.d(TAG, "file: " + uri.toString(), e);
-          return false;
+            Log.d(TAG, "file: " + file.getAbsolutePath(), e);
+            return false;
         } catch (SAXException e) {
-          Log.d(TAG, "file: " + uri.toString(), e);
-          return false;
+            Log.d(TAG, "file: " + file.getAbsolutePath(), e);
+            return false;
         } catch (IOException e) {
-          Log.d(TAG, "file: " + uri.toString(), e);
-          return false;
+            Log.d(TAG, "file: " + file.getAbsolutePath(), e);
+            return false;
         }
-
-      }
-
-    } catch (FileNotFoundException e) {
-
-      //
-
-    } finally {
-
-      if (is != null)
-        try {
-
-          is.close();
-
-        } catch (IOException ignore) {
-
-        }
-
     }
 
-    return false;
-  }
+    private boolean importUri(final Uri uri) {
+        InputStream is = null;
+
+        try {
+
+            final ParcelFileDescriptor pFD = importActivity.getContentResolver().openFileDescriptor(uri,
+                    "r");
+
+            if (pFD != null) {
+
+                is = new FileInputStream(pFD.getFileDescriptor());
+
+                try {
+                    int minRecordingDistance = PreferencesUtils.getInt(importActivity,
+                            R.string.min_recording_distance_key, PreferencesUtils.MIN_RECORDING_DISTANCE_DEFAULT);
+                    long trackIds[] = GpxImporter.importGPXFile(is, myTracksProviderUtils,
+                            minRecordingDistance);
+                    int length = trackIds.length;
+                    if (length > 0) {
+                        trackId = trackIds[length - 1];
+                    }
+                    return true;
+                } catch (FileNotFoundException e) {
+                    Log.d(TAG, "file: " + uri.toString(), e);
+                    return false;
+                } catch (ParserConfigurationException e) {
+                    Log.d(TAG, "file: " + uri.toString(), e);
+                    return false;
+                } catch (SAXException e) {
+                    Log.d(TAG, "file: " + uri.toString(), e);
+                    return false;
+                } catch (IOException e) {
+                    Log.d(TAG, "file: " + uri.toString(), e);
+                    return false;
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+
+            //
+
+        } finally {
+
+            if (is != null)
+                try {
+
+                    is.close();
+
+                } catch (IOException ignore) {
+
+                }
+
+        }
+
+        return false;
+    }
 }
 
